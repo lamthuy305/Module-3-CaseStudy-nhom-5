9a138ad48e937dc9d663cee8baedbdef8a3699e1
bachngocng <nnbach97@gmail.com>
2022-03-09 16:29:19 +0700

    bach cm

--- src/main/java/com/codegym/model/User.java
+++ src/main/java/com/codegym/model/User.java
@@ -9,25 +9,28 @@ public class User {
     private Date birthday;
     private String address;
     private String email;
+    private int role_id;
 
     public User() {
     }
 
-    public User(int id, String username, String password, Date birthday, String address, String email) {
+    public User(int id, String username, String password, Date birthday, String address, String email,int role_id) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.birthday = birthday;
         this.address = address;
         this.email = email;
+        this.role_id = role_id;
     }
 
-    public User(String username, String password, Date birthday, String address, String email) {
+    public User(String username, String password, Date birthday, String address, String email, int role_id) {
         this.username = username;
         this.password = password;
         this.birthday = birthday;
         this.address = address;
         this.email = email;
+        this.role_id = role_id;
     }
 
     public int getId() {
@@ -76,5 +79,13 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public int getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
     }
 }
